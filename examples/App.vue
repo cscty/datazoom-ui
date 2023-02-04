f5f876cd4702ab5ec0693dfa047865ccff742fad
cscty <gdufcscty@163.com>
2023-02-04 17:01:40 +0800

    fix: icon

--- examples/App.vue
+++ examples/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <!-- <dz-button size="large"></dz-button>
+    <dz-button size="large"></dz-button>
     <dz-button></dz-button>
     <dz-button size="small"></dz-button>
-    <dz-button size="default"></dz-button> -->
+    <dz-button size="default"></dz-button>
     <dz-icon></dz-icon>
 </template>
